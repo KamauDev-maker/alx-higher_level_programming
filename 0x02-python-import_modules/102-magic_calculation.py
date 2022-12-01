8aef56eda4dd8ec225ad75350a0f33c26509bc85
KamauDev-maker <oscarnjenga@gmail.com>
2022-12-01 21:35:51 +0300

    new updates

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-def magic_calculation(a, b, c):
+from magic_calculation_102 import add, sub
+def magic_calculation(a, b):
     if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
         return c
-    if b < c:
-        return a + b
-    return a * b - c
+    else:
+        return sub(a, b)
+    return 0
