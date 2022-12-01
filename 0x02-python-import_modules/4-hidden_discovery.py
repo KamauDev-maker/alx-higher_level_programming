27001b4a822c2c8e7cb8a25e9871aa490f801613
KamauDev-maker <oscarnjenga@gmail.com>
2022-12-01 17:35:00 +0300

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    for name in dir(hidden_4):
+        if "__" not in name:
             print(name)
