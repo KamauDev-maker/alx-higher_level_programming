c2eaa5d6b03a57bcd8cfd0dfdc4d5e8e53bd1aee
KamauDev-maker <oscarnjenga@gmail.com>
2022-12-07 17:29:53 +0300

    roman conversion

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -16,7 +16,7 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if roman.get(roman_string[i], 0) == 0:
             return (0)
-        if (i != (len(roman_string) - 1) and 
+        if (i != (len(roman_string) - 1) and
                 roman[roman_string[i]] < roman[roman_string[i + 1]]):
             num += roman[roman_string[i]] * -1
         else:
