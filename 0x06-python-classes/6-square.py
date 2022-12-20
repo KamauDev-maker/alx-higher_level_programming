96dbaa90647704e492323885df5cd08d276296e9
KamauDev-maker <oscarnjenga@gmail.com>
2022-12-20 23:04:23 +0300

    coordinates of square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,15 +6,14 @@ no module imported
 
 class Square:
     """
-    class that generate new instances of square
+    class that generates new square instances
     """
     def __init__(self, size=0, position=(0, 0)):
         """
-        method that helps us define properties for object summary
-
+        method that helps us define properties for our object summary
         Args:
-            size (int): The size of the new square
-            position (int): position of the new square
+            size (int): size of the new square
+            position (int): position of new square
         """
         self.size = size
         self.position = position
@@ -22,53 +21,53 @@ class Square:
     @property
     def size(self):
         """
-        method that helps retrieve new instance
+        method that retrieves the new instance
         """
-        return self.__size
+        return self._size
 
     @size.setter
     def size(self, value):
         """
-        method that helps set the new instance
+        method that sets the new instance
         """
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+            raise ValueError('size must be >= 0')
+        self._size = value
 
     @property
     def position(self):
         """
-        method that helps retrieve new instance position
+        method that retrieves the new position instance
         """
-        return self.__position
+        return self._position
 
     @position.setter
     def position(self, value):
         """
-        method that helps set the new instance position
+        method that retrieves the new position instance
         """
-        if not isinstance(value, tuple) or 
-        len(value) != 2 or not all(isinstance(i, int) and 
-                i >= 0 for i in value):
-            raise TypeError("position must be a tuple of 2 positive integes")
-        self.__position = value
+        if not isinstance(value, tuple) or len(value) != 2
+        or not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self._position = value
 
     def area(self):
         """
-        methods returns the area of the square
+        method that gets the area of a square
         """
-        return self.__size ** 2
+        return self.size ** 2
 
     def my_print(self):
         """
-        prints the square with chacter #
+        methods that prints the square of character #
+        and the position
         """
         if self.size == 0:
             print()
             return
-        for i in range(self.position[i]):
+        for i in range(self.position[1]):
             print()
         for i in range(self.size):
-            print('' * self.position[0] + '#' * self.size)
+            print(' ' * self.position[0] + '#' * self.size)
