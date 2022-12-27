274be78f238530152922d77e09b5b1e9555489e9
KamauDev-maker <oscarnjenga@gmail.com>
2022-12-28 00:04:36 +0300

    divide matrix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,16 +5,17 @@ No modules imported
 
 
 def add_integer(a, b=98):
-"""
-Returns an integer addition of a and b
+    """
+    Returns an integer addition of a and b
 
-Float are to casted to integers
+    Float are to be casted to integers
 
-Raises:
-    TypeError: if a and b are not integers and floats
-"""
-if not (isinstance(a, (int, float)) and\
-        isinstance(b, (int, float))):
+    Raises:
+        TypeError: if a and b are not integers or floats
+    """
+
+
+if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
     raise TypeError("a and b must be integers or floats")
 a = int(a)
 b = int(b)
