31b0a634816397564797008393be0068ebd8cd3c
KamauDev-maker <oscarnjenga@gmail.com>
2023-01-09 10:35:24 +0300

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Gets the list of attributes and methods
+"""
+def lookup(obj):
+    """
+    Gets the list of attributes and methods
+    and returns the list
+    """
+    return dir(obj)
