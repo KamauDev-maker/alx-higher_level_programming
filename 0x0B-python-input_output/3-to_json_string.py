09b2dae126851895e4d50285392db211a80e811c
KamauDev-maker <oscarnjenga@gmail.com>
2023-01-10 12:19:32 +0300

    Json string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,7 +7,7 @@ import json
 
 def to_json_string(my_obj):
     """
-    method that takes an object and converts it to 
+    method that takes an object and converts it to
     a JSON formatted string
     """
     return json.dumps(my_obj)
