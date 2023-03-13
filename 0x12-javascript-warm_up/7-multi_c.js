7bf601ea9abc5bb7b1c1a550d616690d96e644c1
KamauDev-maker <oscarnjenga@gmail.com>
2023-03-13 17:27:39 +0300

    parseInt

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 const num = parseInt(process.argv[2]);
 
 if (isNaN(num)) {
-console.log('Missing number of occurremces');
+  console.log('Missing number of occurremces');
 } else {
-for (let i = 0; i < num; i++) {
-console.log('C is fun');
-}
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
 }
