09eccdad6df12106cbe7c803d4e1dfa57769a26e
KamauDev-maker <oscarnjenga@gmail.com>
2023-04-25 20:02:31 +0300

    starswars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,18 +3,21 @@ const request = require('request');
 const apiUrl = process.argv[2];
 
 request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.log('code: ', response.statusCode);
-  } else {
-    const jsonBody = JSON.parse(body);
-    let count = 0;
-    for (const film of jsonBody.results) {
-      for (const character of film.characters) {
-        if (character.endswith('/18/')) {
-          count += 1;
-        }
-      }
-    }
-    console.log(count);
-  }
+	if (error) {
+		console.log(error);
+	} else if (response.statusCode == 200) {
+		const films = JSON.parse(body).results;
+		let count = 0;
+		for (const film in films) {
+			const filmChars = films[film].characters;
+			for (const charIndex in filmChars) {
+				if (filmChars[charsIndex].includes('18')) {
+					count++;
+				}
+			}
+		}
+		console.log(count);
+	} else {
+		console.log('An error occured. Status code: ' + response.statusCode);
+	}
 });
