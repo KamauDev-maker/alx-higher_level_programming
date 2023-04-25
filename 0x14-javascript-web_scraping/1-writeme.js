6db37bdc1ea036626d08c78dfea7cd0f05c463ce
KamauDev-maker <oscarnjenga@gmail.com>
2023-04-25 17:55:26 +0300

    corrections write a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 const filePath = process.argv[2];
-const string = process.argv[3];
+const str = process.argv[3];
 
-fs.writeFile(filePath, string, 'utf-8', function (err) => {
-  if (err)  console.error(err);
-  
+fs.writeFile(filePath, str, 'utf8', (err) => {
+  if (err) {
+    return console.error(err);
+  }
 });
