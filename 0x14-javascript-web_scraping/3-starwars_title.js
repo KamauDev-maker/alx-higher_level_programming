5559082bd44dd3d010c2e5ae71673489e735fc22
KamauDev-maker <oscarnjenga@gmail.com>
2023-04-25 18:34:46 +0300

    corrections

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-request.get(apiUrl, (error, response, body) => {
+request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-  const epsNum = parseInt(movieData.episode_id);
-
-  if (epsNum === parseInt(movieId)) {
-    console.log(`Title: ${movieData.title}`);
   } else {
-    console.log(`Error code: ${response.statusCode}`);
+    if (response.statusCode === 200) {
+      console.log(JSON.parse(body).title);
+    }
   }
 });
