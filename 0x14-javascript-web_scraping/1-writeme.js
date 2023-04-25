47a929a22f376a73b691632d709f9f9557832186
KamauDev-maker <oscarnjenga@gmail.com>
2023-04-25 17:45:55 +0300

    corrections

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-fs.readFile(filePath, string, 'utf-8', (err) => {
+fs.readFile(filePath, string, 'utf-8', function (err) => {
   if (err) {
     console.error(err);
   }
