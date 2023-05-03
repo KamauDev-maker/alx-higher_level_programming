deb16057e2bc31060a1675044280839e924b2e64
KamauDev-maker <oscarnjenga@gmail.com>
2023-05-03 16:30:03 +0300

    correction

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('div').click(function() {
-		$('#red_header').css('color', '#FF0000');
+	$('#red_header').click(function() {
+		$('header').css('color', '#FF0000');
 	});
 });
